11d7940309fc5b510285c4768a5070b5323f28de
cmabdullah <cmabdullah21@gmail.com>
2017-08-14 23:03:27 +0600

    Hello world

---
+++ SpceShip.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : SpaceShip.cpp
+// Author      : C M Abdullah Khan
+// Version     : 1.1
+// Copyright   : OpenSource
+// Compiler    : Eclipse CDT ( C/C++ Development Tools)
+// Problem     : Project no 3
+// Description : Hello World in C, Ansi-style
+// Date        : 20-07-17
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!!!");
+	return EXIT_SUCCESS;
+}
