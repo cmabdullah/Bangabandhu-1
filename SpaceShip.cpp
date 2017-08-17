663ecffc3972014e9c556f4d545ebbfceb89e9bf
cmabdullah <cmabdullah21@gmail.com>
2017-08-17 17:49:16 +0600

    Rocket drow complete

--- SpaceShip.cpp
+++ SpaceShip.cpp
@@ -132,66 +132,119 @@ g++ -L/usr/X11/lib -arch i386 -o  "SpceShip"  "SpceShip.o"  -lglu -lgl -lglut
  * op2
  * op3
  * */
+/**
+-+,xy	|	++
+		|
+--------------------
+		|
+--		|	+-
+***/
 
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
 #include <GL/glut.h>
 #endif
+#include<math.h>
 
+#define PI 3.1416
 
-
-float b_animx=0;
-float b_animy=0;
-float b2_animx=0;
-float b2_animy=0;
-void rectangle( float r, float g, float b, float fx, float fy,float sx, float sy, float tx, float ty, float fox, float foy)
-{
+float animx=0;
+float animy=0;
+void rectangle( float r, float g, float b,  float ulx, float uly, float urx, float ury,  float brx, float bry,  float blx, float bly){
     glColor3f(r,g,b);
     glBegin(GL_POLYGON);
-    glVertex2f(fx,fy);
-    glVertex2f(sx,sy);
-    glVertex2f(tx,ty);
-    glVertex2f(fox,foy);
+    glVertex2f(ulx,uly);
+    glVertex2f(urx,ury);
+    glVertex2f(brx,bry);
+    glVertex2f(blx,bly);
     glEnd();
 }
 
+void triangle( float r, float g, float b, float lx, float ly,float rx, float ry, float tbx, float tby){
+    glColor3f(r,g,b);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(lx,ly);
+    glVertex2f(rx,ry);
+    glVertex2f(tbx,tby);
+    glEnd();
+}
+
+void frem(){
+	rectangle(0.4,0.0,0.0, 0.01,-0.80,  0.02,-0.81, 0.02,-0.99, 0.01,-0.99);
+	rectangle(0.4,0.0,0.0, 0.01,-0.80,  0.3,-0.80, 0.29,-0.81, 0.02,-0.81);
+	rectangle(0.4,0.0,0.0, 0.29,-0.81,  0.30,-0.80, 0.30,-0.99, 0.29,-0.99);
+}
+void boat2(){
+
+	//body
+	rectangle(0.4,0.0,0.0, 0.10,-0.70,  0.20,-0.70, 0.20,-0.90, 0.10,-0.90);
+	//bonet
+	triangle(0,0.5,0.5,   0.10,-0.7,   0.20,-0.7,   0.15,-0.57);
+	//Wings right top
+	triangle(0,0.5,0.5,   0.20,-0.75,   0.25,-0.75,   0.20,-0.70);
+	//Wings left top
+	triangle(0,0.5,0.5,   0.05,-0.75,   0.10,-0.75,   0.10,-0.70);
+	//Wings right bottom
+	triangle(0,0.5,0.5,   0.20,-0.90,   0.25,-0.93,   0.20,-0.82);
+	//Wings left bottom
+	triangle(0,0.5,0.5,   0.05,-0.93,   0.10,-0.90,   0.10,-0.82);
+	//Exhaust left
+	triangle(0,0.5,0.5,   0.10,-0.90,   0.12,-0.90,   0.11,-0.95);
+	//Exhaust right
+	triangle(0,0.5,0.5,   0.18,-0.90,   0.20,-0.90,   0.19,-0.95);
+	//Exhaust middel
+	triangle(0,0.5,0.5,   0.13,-0.90,   0.17,-0.90,   0.15,-0.97);
 
 
-void boat1()
-{
+
+}
+void box2(){
+	rectangle(0.4,0.5,0.60, 0.20,-0.60, 0.20,-0.70, 0.30,-0.70, 0.30,-0.60);
+}
+
+
+void box(){
     glPushMatrix();
-    glTranslated(0,0.1,0);
-    glScaled(0.9,0.9,0);
-    rectangle(0.4,0.5,0.60, 0.08,0.51, 0.20,0.76, 0.52,0.55, 0.39,0.34);
+    glTranslated(0,0,0);
+    glScaled(1,1,0);
+    rectangle(0.4,0.5,0.60, 0.20,-0.60, 0.20,-0.70, 0.30,-0.70, 0.30,-0.60);
     glPopMatrix();
 }
+
 void init(){
 	//screen color setup
 	    glClearColor(0.0,0.0,0.0,1.0);
-	    glClear(GL_COLOR_BUFFER_BIT);
-	glOrtho(0,1,0,1,0,1);
+	    //glClear(GL_COLOR_BUFFER_BIT);
+	glOrtho(0,1, -1,0, 0,1);
 }
 void myDisplay(){
 
     glClear(GL_COLOR_BUFFER_BIT);
+    frem();
+    box2();
+    boat2();
+
 
     glPushMatrix();
-    glTranslated(b2_animx,b2_animy,0);
-    boat1();
+    glTranslated(animx,animy,0);
+    box();
     glPopMatrix();
-    if(b2_animx>=-0.7){
-         b2_animx=b2_animx-0.00005;
+
+    if(animy<=0.5){
+    	animy=animy+0.0005;
     }
        glutPostRedisplay();
+
 	glFlush();
+
 }
 
 int main(int argc,char*argv[]){
 glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(1024,720);
-	glutInitWindowPosition(1024,720);
+	glutInitWindowSize(466,466);
+	glutInitWindowPosition(50,50);
 	glutCreateWindow("Demo App");
 	init();
 	glutDisplayFunc(myDisplay);
