372eae937ef782ae5113a8514a73690e214d2cf6
cmabdullah <cmabdullah21@gmail.com>
2017-08-21 23:37:43 +0600

    Fixed some bug

--- SpaceShip.cpp
+++ SpaceShip.cpp
@@ -151,6 +151,21 @@ g++ -L/usr/X11/lib -arch i386 -o  "SpceShip"  "SpceShip.o"  -lglu -lgl -lglut
 
 float animx=0;
 float animy=0;
+float animxT=0;
+float animyT=0;
+float animxB=0;
+float animyB=0;
+void circle(float r, float g, float b, float x, float y, float init, float last, float radx, float rady){
+    glColor3f(r,g,b);
+    glBegin(GL_POLYGON);
+    for(float i=init;i<=last;i++)
+    {
+        float j=(i*PI)/180;
+        glVertex2f(x+(radx*cos(j)),y+(rady*sin(j)));
+    }
+    glEnd();
+
+}
 void rectangle( float r, float g, float b,  float ulx, float uly, float urx, float ury,  float brx, float bry,  float blx, float bly){
     glColor3f(r,g,b);
     glBegin(GL_POLYGON);
@@ -175,16 +190,25 @@ void frem(){
 	rectangle(0.4,0.0,0.0, 0.01,-0.80,  0.3,-0.80, 0.29,-0.81, 0.02,-0.81);
 	rectangle(0.4,0.0,0.0, 0.29,-0.81,  0.30,-0.80, 0.30,-0.99, 0.29,-0.99);
 }
-void boat2(){
+void rocketTop(){
 
-	//body
-	rectangle(0.4,0.0,0.0, 0.10,-0.70,  0.20,-0.70, 0.20,-0.90, 0.10,-0.90);
+	//body top
+	rectangle(0.4,0.0,0.0, 0.10,-0.70,  0.20,-0.70, 0.20,-0.82, 0.10,-0.82);
+
 	//bonet
 	triangle(0,0.5,0.5,   0.10,-0.7,   0.20,-0.7,   0.15,-0.57);
 	//Wings right top
 	triangle(0,0.5,0.5,   0.20,-0.75,   0.25,-0.75,   0.20,-0.70);
 	//Wings left top
 	triangle(0,0.5,0.5,   0.05,-0.75,   0.10,-0.75,   0.10,-0.70);
+	//body bottom
+
+}
+
+void rocketBottom(){
+
+	//body bottom
+	rectangle(0.4,1.0,1.0, 0.10,-0.82,  0.20,-0.82, 0.20,-0.90, 0.10,-0.90);
 	//Wings right bottom
 	triangle(0,0.5,0.5,   0.20,-0.90,   0.25,-0.93,   0.20,-0.82);
 	//Wings left bottom
@@ -195,22 +219,87 @@ void boat2(){
 	triangle(0,0.5,0.5,   0.18,-0.90,   0.20,-0.90,   0.19,-0.95);
 	//Exhaust middel
 	triangle(0,0.5,0.5,   0.13,-0.90,   0.17,-0.90,   0.15,-0.97);
+}
 
+void field(){
+	rectangle(0.957, 0.643, 0.376, 0.01,-0.66, 0.99,-0.66, 0.99,-0.99, 0.01,-0.99);
+}
+void sky(){
+	rectangle(0.000, 1.000, 0.000, 0.01,-0.66, 0.99,-0.66, 0.99,-0.01, 0.01,-0.01);
+}
+void DarkSky(){
+	rectangle(0.0, 0.0, 0.0, 0.01,-0.01, 0.99,-0.01, 0.99,-0.99, 0.01,-0.99);
+}
+void house(){
+	rectangle(0.0,1.5,1.60, 0.85,-0.57, 0.93,-0.57, 0.93,-0.66, 0.85,-0.66);
+}
 
+void tree(){
+	rectangle(0.0,1.5,1.60,  0.69,-0.60, 0.71,-0.60, 0.71,-0.66, 0.69,-0.66);
+	triangle(0,0.5,0.5,   0.71,-0.60,   0.72,-0.59,   0.75,-0.59);
+	triangle(0,0.5,0.5,   0.69,-0.60,   0.71,-0.60,   0.70,-0.55);
+	triangle(0,0.5,0.5,   0.69,-0.60,   0.68,-0.59,   0.65,-0.59);
 
 }
-void box2(){
-	rectangle(0.4,0.5,0.60, 0.20,-0.60, 0.20,-0.70, 0.30,-0.70, 0.30,-0.60);
-}
+
 
 
 void box(){
     glPushMatrix();
     glTranslated(0,0,0);
-    glScaled(1,1,0);
+    glScaled(1,.8,0);
     rectangle(0.4,0.5,0.60, 0.20,-0.60, 0.20,-0.70, 0.30,-0.70, 0.30,-0.60);
     glPopMatrix();
 }
+void box2(){
+	rectangle(0.4,0.5,0.60, 0.20,-0.60, 0.20,-0.70, 0.30,-0.70, 0.30,-0.60);
+}
+void box3(){
+	glPushMatrix();
+	box();
+	glTranslated(0,0.1,0);
+	glPopMatrix();
+}
+
+
+void box4(){
+	glPushMatrix();
+	box();
+	glTranslated(0,0.3,0);
+	glPopMatrix();
+}
+
+
+void rocketMotion(){
+	glPushMatrix();
+    rocketTop();
+    rocketBottom();
+	glTranslated(0,0,0);
+	glPopMatrix();
+}
+
+void rocketMotionU(){
+	glPushMatrix();
+    rocketTop();
+    rocketBottom();
+	glTranslated(0,0.3,0);
+	glPopMatrix();
+}
+
+void rocketMotionD(){
+	glPushMatrix();
+   // rocketTop();
+    rocketBottom();
+	glTranslated(0,0.3,0);
+	glPopMatrix();
+}
+
+
+void sun()
+{
+    circle(01,0,0,  0.78,-0.18,1,500,0.1,0.1);
+}
+
 
 void init(){
 	//screen color setup
@@ -218,23 +307,119 @@ void init(){
 	    //glClear(GL_COLOR_BUFFER_BIT);
 	glOrtho(0,1, -1,0, 0,1);
 }
+
+
+
+
+
+
+
+
 void myDisplay(){
 
     glClear(GL_COLOR_BUFFER_BIT);
-    frem();
+    field();
+    sky();
+    house();
     box2();
-    boat2();
+    frem();
+
+
+    glPushMatrix();
+    glTranslated(-0.1,0,0);
+    tree();
+    glTranslated(-0.1,0,0);
+    tree();
+    glTranslated(0.29,0,0);
+        tree();
+    glTranslated(-0.06,-0.3,0);
+        tree();
+    glTranslated(0.2,-0.01,0);
+        tree();
+    glPopMatrix();
 
 
     glPushMatrix();
     glTranslated(animx,animy,0);
-    box();
+    rocketTop();
+    rocketBottom();
     glPopMatrix();
-
-    if(animy<=0.5){
+    if(animy<=.1){
     	animy=animy+0.0005;
     }
        glutPostRedisplay();
+
+    /* sky changes*/
+    if(animy<=.1){
+        // rectangle(0.678, 0.847, 0.902, 50,40, 50,-40, -50,-40,-50,40);
+    	sun();
+
+    }
+    else{
+
+    		DarkSky();
+
+            glPushMatrix();
+            //animy = 0;
+            glTranslated(animx,animy,0);
+            rocketTop();
+            rocketBottom();
+            glPopMatrix();
+            	if(animy<=.2){
+                	animy=animy+0.0005;
+             }
+
+
+                glPushMatrix();
+                glTranslated(animx,animy,0);
+                rocketMotionU();
+                glPopMatrix();
+                	if(animy<=.3){
+                    	animy=animy+0.0005;
+                    	animyB=animyB+0.0005;
+                 }
+
+	                glPushMatrix();
+	                glTranslated(animxB,animyB,0);
+	                rocketMotionD();
+	                glPopMatrix();
+	                	if(animyB>=.3){
+	                    	animyB=animyB-0.0005;
+	                 }
+
+
+        }
+
+/***
+
+
+       glPushMatrix();
+       glTranslated(animx,animy,0);
+       box();
+       glPopMatrix();
+       if(animy<=.1){
+       	animy=animy+0.0005;
+       }
+
+
+       glPushMatrix();
+       glTranslated(animx,animy,0);
+       box3();
+       glPopMatrix();
+       	if(animy<=.2){
+           	animy=animy+0.0005;
+        }
+
+        glPushMatrix();
+        glTranslated(animx,animy,0);
+        box4();
+        glPopMatrix();
+        	if(animy<=.3){
+            	animy=animy+0.0005;
+         }
+
+***/
+       	glutPostRedisplay();
 
 	glFlush();
 
